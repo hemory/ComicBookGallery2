15c35a4f6b9efda536ab2f53e69078f3f1a3cd28
Hemory Phifer <hemphife@gmail.com>
2020-12-23 11:06:36 -0500

    Add project files.

---
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,42 @@
+﻿using ComicBookGallery.data;
+using ComicBookGallery.Models;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+
+            return View(comicBooks);
+        }
+
+        // GET: ComicBooks
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            ComicBook comicBook = _comicBookRepository.GetComicBook(id.Value);
+
+
+            return View(comicBook);
+        }
+
+
+    }
+
+
+}
---
+++ ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,16 @@
+﻿namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHTML { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+        public string DisplayText => $"{SeriesTitle} #{IssueNumber}";
+
+        public string CoverImageFileName => $"{SeriesTitle.Replace(" ", "-").ToLower()}-{IssueNumber}.jpg";
+    }
+}
